33277654eb3f77bbb964dc6d334fff46368a64a1
pkp-ruet <pritompaul007@gmail.com>
2023-01-23 22:52:27 +0600

    with love

---
+++ Love/another-blog-in-the-wall/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Other components and content */}
+    </div>
+  );
+}
+
+export default App;
